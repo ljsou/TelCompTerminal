0c34c29391399d9b4c869d081c20118c6baae70d
Javier Suarez <luisjavier.suarezmeza@gmail.com>
2013-09-11 18:51:54 -0500

    Implementadas las funcionalidades Show, Erase, y Find

--- src/java/org/telcomp/terminal/ControlJslee.java
+++ src/java/org/telcomp/terminal/ControlJslee.java
@@ -5,6 +5,7 @@
 package org.telcomp.terminal;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import webservice.JSLEEorchestrator_Service;
 
@@ -23,15 +24,38 @@ public class ControlJslee {
         return deleteService.getJSLEEorchestratorPort().deleteService(arg0);
     }
 
-    private static boolean searchServiceOnJSLEE() {
+    private static List<String> searchServicesOnJSLEE() {
         boolean exist = false;
         System.out.println("Searching... ");
         webservice.JSLEEorchestrator_Service deleteService = new JSLEEorchestrator_Service();
         List<String> deployedServices = deleteService.getJSLEEorchestratorPort().getDeployedServices();
-        for (String service : deployedServices) {
-            System.out.println(service);
+        return deployedServices;
+    }
+
+    private static boolean searchServiceOnJSLEE(java.lang.String arg0) {
+        boolean exist = false;
+        System.out.println("Searching... ");
+        webservice.JSLEEorchestrator_Service deleteService = new JSLEEorchestrator_Service();
+        List<String> deployedServices = deleteService.getJSLEEorchestratorPort().getDeployedServices();
+        for (Iterator<String> it = deployedServices.iterator(); it.hasNext();) {
+            String string = it.next();
+            if (string.equalsIgnoreCase(arg0)){
+                return true;
+            }
         }
-        return exist;
+        return false;
+    }
+
+    private static boolean deleteAllServicesOnJSLEE() {
+        boolean result = false;
+        System.out.println("Searching... ");
+        webservice.JSLEEorchestrator_Service deleteService = new JSLEEorchestrator_Service();
+        List<String> deployedServices = deleteService.getJSLEEorchestratorPort().getDeployedServices();
+        for (Iterator<String> it = deployedServices.iterator(); it.hasNext();) {
+            String string = it.next();
+            result = deleteServiceOnJSLEE(string);
+        }
+        return result;
     }
 
     private static String setJsonGraphToJSEEOrchestrate(java.lang.String arg0, String name) {
@@ -44,13 +68,18 @@ public class ControlJslee {
         System.out.println("Removing service: " + serviceName);
         return deleteServiceOnJSLEE(serviceName);
     }
-    
-    public void removeAll(){
-        
+
+    public String removeAll() {
+        String remove = deleteAllServicesOnJSLEE() ? "Services deleted" : "Services were not eliminated";
+        return remove;
     }
-    
-    public void searchAll(){
+
+    public List<String> searchAll() {
         System.out.println("Search all...");
-        searchServiceOnJSLEE();
+        return searchServicesOnJSLEE();
+    }
+
+    public boolean findService(String serviceName) {
+        return searchServiceOnJSLEE(serviceName);
     }
 }
--- src/java/org/telcomp/terminal/TerminalController.java
+++ src/java/org/telcomp/terminal/TerminalController.java
@@ -27,17 +27,40 @@ public class TerminalController {
         String showTerminal = "<a style=\"color: " + color + "; font-weight: 600;\">" + content + "</a>";
         return showTerminal;
     }
+    
+    public String addSpace(int n){
+        String space = "";
+        for (int i = 0; i < n; i++) {
+            space = space + "&nbsp;";
+        }
+        return space;
+    }
+            
 
     public String handleCommand(String command, String[] params) {
         if (command.equals("help")) {
-            String content = "\"rm\"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Allows you to delete a service deployed, rm &lt;Service name&gt;";
-            String showTerminal = setCSS(content, "brown");
+            String rm = "\"rm\"" + addSpace(10) + "Allows you to delete a service deployed, rm &lt;Service name&gt;";
+            String show = "\"show\"" + addSpace(6) + "Shows you all the deployed services";
+            String erase = "\"erase\"" + addSpace(5) + "Removes all deployed services, rm &lt;Service name&gt;";
+            String find = "\"find\"" + addSpace(8) + "Finds a deployed service, find &lt;Service name&gt;";
+            String content = rm + "<br />" + erase + "<br />" + show + "<br />" + find ;
+            String showTerminal = setCSS(content, "orange");
             return showTerminal;
         } else if (command.equals("rm")) {
             if (params.length > 0) {
                 boolean remove = cj.removeService(params[0]);
-                String tResult = setCSS(params[0] + " deleted.", "chartreuse");
-                String fResult = setCSS(params[0] + " not found.", "red");
+                String tResult = setCSS(params[0] + " deleted.", "green");
+                String fResult = setCSS(params[0] + " service not found.", "red");
+                String result = remove ? tResult : fResult;
+                return result;
+            } else {
+                return params[0] + "not found.";
+            }
+        } else if (command.equals("find")) {
+            if (params.length > 0) {
+                boolean remove = cj.findService(params[0]);
+                String tResult = setCSS(params[0] + " service exists", "green");
+                String fResult = setCSS(params[0] + " service not found.", "red");
                 String result = remove ? tResult : fResult;
                 return result;
             } else {
@@ -45,6 +68,10 @@ public class TerminalController {
             }
         } else if (command.equals("date")) {
             return new Date().toString();
+        } else if (command.equals("show")) {
+            return setCSS("" + cj.searchAll(), "black");
+        } else if (command.equals("erase")) {
+            return setCSS("" + cj.removeAll(), "blue");
         } else {
             return command + " not found";
         }
